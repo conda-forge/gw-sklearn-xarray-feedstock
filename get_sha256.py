d4f76cf730f01bc09231eb1e5c7a322a97ef972b
mmann1123 <mmann1123@gmail.com>
2024-07-26 14:05:55 -0400

    resolved scikit-learn pin requirement

---
+++ get_sha256.py
@@ -0,0 +1,17 @@
+# %%
+import requests
+import hashlib
+
+
+def get_sha256_hash(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure the request was successful
+    sha256_hash = hashlib.sha256(response.content).hexdigest()
+    return sha256_hash
+
+
+url = "https://github.com/mmann1123/sklearn-xarray/archive/refs/tags/0.5.1.tar.gz"
+hash_value = get_sha256_hash(url)
+print(f"SHA-256 hash: {hash_value}")
+
+# %%
